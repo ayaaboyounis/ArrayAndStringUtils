4ed22dc71a82db29e587033ba832d24e24a64711
aya aboyounis <71427068+ayaaboyounis@users.noreply.github.com>
2022-06-21 22:12:13 +0300

    Add files via upload

---
+++ il/ac/tau/cs/sw1/hw3/ArrayUtils.java
@@ -0,0 +1,33 @@
+package il.ac.tau.cs.sw1.hw3;
+
+public class ArrayUtils {
+	
+	public static int[][] transposeMatrix(int[][] m)
+	{
+		//TODO
+		return m;
+		
+	}
+	
+	public static int[] shiftArrayToTheLeftCyclic(int[] array, int move)
+	{
+		//TODO
+		return array;
+		
+	}
+	
+	public static int alternateSum(int[] array)
+	{
+		//TODO
+		return 0;
+		
+	}
+	
+	public static int findPath(int[][] m, int i, int j)
+	{
+		//TODO
+		return 0;
+		
+	}
+
+}
---
+++ il/ac/tau/cs/sw1/hw3/StringUtils.java
@@ -0,0 +1,25 @@
+package il.ac.tau.cs.sw1.hw3;
+
+public class StringUtils {
+	
+	public static String sortStringWords (String str)
+	{
+		//TODO
+		return str;
+		
+	}
+
+	public static String xorStrings(String a, String b)
+	{
+		//TODO
+		return b;
+		
+	}
+	
+	public static boolean isAnagram(String a, String b)
+	{
+		//TODO
+		return false;
+		
+	}
+}
